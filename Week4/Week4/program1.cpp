bda58df70a6395d7dc42ae51a9b772513972a3f9
saurav-chand22 <chandsourav099@gmail.com>
2026-05-21 15:10:16 +0530

    Update program1.cpp

--- Week4/Week4/program1.cpp
+++ Week4/Week4/program1.cpp
@@ -1,1 +1,99 @@
+#include <iostream>
+using namespace std;
 
+long long comparisons = 0;
+long long inversions = 0;
+
+void merge(int arr[], int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    for(int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+
+    for(int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while(i < n1 && j < n2)
+    {
+        comparisons++;
+
+        if(L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            inversions += (n1 - i);
+            j++;
+        }
+        k++;
+    }
+
+    while(i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while(j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right)
+{
+    if(left < right)
+    {
+        int mid = (left + right) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while(T--)
+    {
+        int n;
+        cin >> n;
+
+        int arr[n];
+
+        for(int i = 0; i < n; i++)
+            cin >> arr[i];
+
+        comparisons = 0;
+        inversions = 0;
+
+        mergeSort(arr, 0, n - 1);
+
+        cout << "Sorted Array: ";
+        for(int i = 0; i < n; i++)
+            cout << arr[i] << " ";
+
+        cout << endl;
+
+        cout << "Comparisons = " << comparisons << endl;
+        cout << "Inversions = " << inversions << endl;
+    }
+
+    return 0;
+}
